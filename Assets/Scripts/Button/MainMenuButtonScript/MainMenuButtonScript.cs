d3b4fd6811dad64caa9d71506e3717ba22e29b75
yossy-o-o <char.aznable02@icloud.com>
2024-11-25 05:00:05 +0900

    add : リザルト画面のメインメニューボタンの実装
    details :
    メインメニューに戻るためのボタンの実装を行った
    
    Assets/Scripts/Button/MainMenuButtonScript に入っている
    
    今後すること :
    
    スタートシーンの完成
    リスタート機能の実装
    UI調整
    デバック

---
+++ Assets/Scripts/Button/MainMenuButtonScript/MainMenuButtonScript.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuButtonScript : MonoBehaviour
+{
+    public void OnClickMainMenuButton()
+    {
+        SceneManager.LoadScene("StartScene");
+    }
+}
